60ea5ee597069960a9fddef5baa1deaf7e6b3abb
GabrieuGarcia <contato.gabrielgarcia@gmail.com>
2022-11-15 12:46:35 +0000

    feature/SP- create project

---
+++ backend/src/test/java/com/gbl/salesproject/SalesprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbl.salesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
