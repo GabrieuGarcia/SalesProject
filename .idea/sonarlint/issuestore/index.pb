a0a9c3a441d4c395fc02c9bfec153591f2b62a8c
GabrieuGarcia <contato.gabrielgarcia@gmail.com>
2022-11-18 12:56:25 +0000

    feature/SP-Date select

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+j
+:backend/src/main/java/com/gbl/salesproject/model/Sale.java,7/8/78a61ddfcb5e5064ab03ad21461281b4b5ac70e6
--- backend/src/main/java/com/gbl/salesproject/service/SaleService.java
+++ backend/src/main/java/com/gbl/salesproject/service/SaleService.java
@@ -3,9 +3,13 @@ package com.gbl.salesproject.service;
 import com.gbl.salesproject.model.Sale;
 import com.gbl.salesproject.repository.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleService {
@@ -13,7 +17,12 @@ public class SaleService {
     @Autowired
     private SaleRepository saleRepository;
 
-    public List<Sale> findSales() {
-        return saleRepository.findAll();
+    public Page<Sale> findAll(String minDate, String maxDate, Pageable pageable) {
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = minDate.equals("") ? today.minusYears(1L) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+        return saleRepository.findAll(min, max, pageable);
     }
 }
